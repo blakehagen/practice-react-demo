ffc58dd6cacdc6d9ff09b52b8dd0d08d27ac399a
Blake Hagen <blakehagen@gmail.com>
2016-08-02 19:21:07 -0600

    initial

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>hello</div>
+        <NavBar />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default NavBar
